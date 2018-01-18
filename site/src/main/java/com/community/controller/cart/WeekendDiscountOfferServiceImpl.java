4dd97378178ece6f29f65c22b844f745b0c649f9
Thomas Briner <thomas.briner@gmail.com>
2018-01-18 10:57:21 +0100

    use testing_time in WeekendDiscountOfferService

--- site/src/main/java/com/community/controller/cart/WeekendDiscountOfferServiceImpl.java
+++ site/src/main/java/com/community/controller/cart/WeekendDiscountOfferServiceImpl.java
@@ -1,5 +1,6 @@
 package com.community.controller.cart;
 
+import com.community.core.testing.TestingTime;
 import org.broadleafcommerce.core.offer.domain.Offer;
 import org.broadleafcommerce.core.offer.service.OfferService;
 import org.broadleafcommerce.core.offer.service.OfferServiceImpl;
@@ -8,16 +9,26 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Priority;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 
 
 @Service("weekendOfferService")
 public class WeekendDiscountOfferServiceImpl extends OfferServiceImpl implements OfferService {
 
+    @PersistenceContext(unitName="blPU")
+    protected EntityManager em;
+
 
     @Override
     public List<Offer> buildOfferListForOrder(Order order) {
         System.out.println("Hello WeekendDiscount");
+
+
+        TestingTime testingTime = em.find(TestingTime.class, new Long(42));
+        System.out.println(testingTime.getTestingTime());
+
         return super.buildOfferListForOrder(order);
     }
 }
